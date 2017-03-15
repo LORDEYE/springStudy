3f102ed9e2d82b5206fd7a2b487a909265bcf2a1
lordeye <lordeye@naver.com>
2017-03-16 01:00:01 +0900

    컨트롤러 제이슨 테스트

---
+++ ex00/src/main/java/com/tistory/lordeye/SampleController5.java
@@ -0,0 +1,19 @@
+package com.tistory.lordeye;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.tistory.lordeye.domain.ProductVO;
+
+@Controller
+public class SampleController5 {
+	
+	@RequestMapping("/doJSON")
+	public @ResponseBody ProductVO doJSON(){
+		ProductVO vo = new ProductVO("»ùÇÃ»óÇ°", 30000);
+		
+		return vo;
+		
+	}
+}
