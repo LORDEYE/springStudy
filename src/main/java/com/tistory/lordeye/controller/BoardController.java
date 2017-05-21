36191e0f9b5e9c47311003e7a5e1d20dcb748bc5
Administrator <Administrator@MSDN-SPECIAL>
2017-05-22 00:41:02 +0900

    수정삭제 작업중 수정페이지 수정 안함

--- src/main/java/com/tistory/lordeye/controller/BoardController.java
+++ src/main/java/com/tistory/lordeye/controller/BoardController.java
@@ -62,4 +62,26 @@ public class BoardController {
 		model.addAttribute(service.read(bno));
 	}
 	
+	@RequestMapping(value="/remove",method = RequestMethod.POST)
+	public String remove(@RequestParam int bno,RedirectAttributes rttr) throws Exception{
+		service.remove(bno);
+		rttr.addFlashAttribute("msg","SUCCESS");
+		
+		
+		return "redirect:/board/listAll";
+	}
+	@RequestMapping(value="/modify", method=RequestMethod.POST)
+	public String modifyPOST(BoardVO board,RedirectAttributes rttr) throws Exception{
+		
+		service.modify(board);
+		rttr.addFlashAttribute("msg", "SUCCESS");
+		return "redirect:/board/listAll";
+	}
+	@RequestMapping(value="/modify" , method = RequestMethod.GET)
+	public void modifyGET(int bno, Model model) throws Exception{
+		model.addAttribute(service.read(bno));
+		
+		
+	}
+	
 }
